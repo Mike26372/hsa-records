0d6010a8eaa8209c37c69f4918df25bb75fe00d6
Michael Tutt <tutt.michael@gmail.com>
2023-10-11 14:59:14 -0700

    Cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import HSATable from "./hsa-table";
 
 type Records = {
-  records: EmployeeRecord[];
+	records: EmployeeRecord[];
 };
 
 export type EmployeeRecord = {
-  id: string;
-  createdTime: string;
-  fields: EmployeeFields;
+	id: string;
+	createdTime: string;
+	fields: EmployeeFields;
 };
 
 export const planTypeFamily = "Family";
@@ -16,39 +16,41 @@ export const planTypeSelf = "Self-only";
 type PlanType = typeof planTypeFamily | typeof planTypeSelf;
 
 type EmployeeFields = {
-  "Plan type": PlanType;
-  Name: string;
-  Deductible: number;
-  "Date of birth": string;
+	"Plan type": PlanType;
+	Name: string;
+	Deductible: number;
+	"Date of birth": string;
 };
 
-async function getEmployeeRecords(): Promise<Records | undefined> {
-  const res = await fetch(
-    "https://api.airtable.com/v0/appekA493GuXz8uDK/tbllLFdZDMfLjAT4N",
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.AIRTABLE_API_KEY}`,
-      },
-    },
-  );
+const airtableApiURL = "https://api.airtable.com/v0/appekA493GuXz8uDK/tbllLFdZDMfLjAT4N"
 
-  try {
-    return res.json();
-  } catch (e) {
-    console.error(e);
-  }
+async function getEmployeeRecords(): Promise<Records | undefined> {
+	const res = await fetch(
+		airtableApiURL,
+		{
+			headers: {
+				Authorization: `Bearer ${process.env.AIRTABLE_API_KEY}`,
+			},
+		},
+	);
+
+	try {
+		return res.json();
+	} catch (e) {
+		console.error(e);
+	}
 }
 
 export default async function Home() {
-  const employeeRecords = await getEmployeeRecords();
+	const employeeRecords = await getEmployeeRecords();
 
-  if (!employeeRecords) {
-    return null;
-  }
+	if (!employeeRecords) {
+		return null;
+	}
 
-  return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <HSATable records={employeeRecords.records} />
-    </section>
-  );
+	return (
+		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+			<HSATable records={employeeRecords.records} />
+		</section>
+	);
 }
